2b4e1211d4f26664339072f038bc81053611a39f
mura-work <murata.naoya.work@gmail.com>
2024-11-04 22:44:37 +0900

    [feat] 企業情報と担当者のエンドポイントを作成

--- src/app.js
+++ src/app.js
@@ -15,6 +15,32 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// 法人名（法人番号）の取得
+app.get("/api/notion-data/client-companies", async (req, res) => {
+  const databaseId = "13182b75468c80678161c6c6912b8d26"; // 本来であれば定数化するのが好ましい、、、
+
+  try {
+    const response = await axios({
+      method: "post",
+      url: `https://api.notion.com/v1/databases/${databaseId}/query`,
+      headers: {
+        Authorization: `Bearer ${process.env.REACT_APP_NOTION_API_KEY}`,
+        "Notion-Version": "2022-06-28",
+        "Content-Type": "application/json",
+      },
+    });
+
+    res.json(response.data);
+  } catch (error) {
+    console.error(
+      "Error querying Notion database:",
+      error.response ? error.response.data : error.message
+    );
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+// 担当者の取得
 app.get("/api/notion-data/sales-employees", async (req, res) => {
   const databaseId = "13482b75468c80ec878aec03efd68241"; // 本来であれば定数化するのが好ましい、、、
 
