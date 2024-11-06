07c0b13f5318f27c44507908466fa278dc481a66
mura-work <murata.naoya.work@gmail.com>
2024-11-06 20:38:27 +0900

    [feat] 商品のエンドポイントの作成

--- src/app.js
+++ src/app.js
@@ -65,6 +65,31 @@ app.get("/api/notion-data/sales-employees", async (req, res) => {
   }
 });
 
+// 商品の取得
+app.get("/api/notion-data/products", async (req, res) => {
+  const databaseId = "13182b75468c80a9b766e570b5383762"; // 本来であれば定数化するのが好ましい、、、
+
+  try {
+    const response = await axios({
+      method: "post",
+      url: `https://api.notion.com/v1/databases/${databaseId}/query`,
+      headers: {
+        Authorization: `Bearer ${process.env.REACT_APP_NOTION_API_KEY}`,
+        "Notion-Version": "2022-06-28",
+        "Content-Type": "application/json",
+      },
+    });
+
+    res.json(response.data);
+  } catch (error) {
+    console.error(
+      "Error querying Notion database:",
+      error.response ? error.response.data : error.message
+    );
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
 // ローカル環境で動作確認したい場合はコメントアウトを外すとサーバーが立ち上がる
 // const PORT = process.env.PORT || 3005;
 // app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
