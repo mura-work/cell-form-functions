60a6cca7e22118fc288cfbd26d9850cf17779eee
mura-work <murata.naoya.work@gmail.com>
2025-01-25 18:26:38 +0900

    シフト管理

--- src/app.js
+++ src/app.js
@@ -90,6 +90,93 @@ app.get("/api/notion-data/products", async (req, res) => {
   }
 });
 
+// シフト管理DBに保存する
+app.post("/api/notion-data/shift-management", async (req, res) => {
+  const shiftRequestValues = req.body.shiftRequestValues;
+  const homeWorkerName = req.body.homeWorkerName;
+  const convertedShifts = shiftRequestValues.map(
+    ({ date, startTime, endTime }) => {
+      // "2025-01-01 09:00" などに結合して返す
+      const startAt = `${date} ${startTime}`;
+      const endAt = `${date} ${endTime}`;
+
+      // 差分をミリ秒で計算
+      const diffMilliseconds = Math.abs(
+        new Date(`${date} ${endTime}`).getTime() -
+          new Date(`${date} ${startTime}`).getTime()
+      );
+
+      // ミリ秒を時間に変換
+      const workTime = diffMilliseconds / (1000 * 60 * 60);
+
+      // Dateオブジェクトを受け取り、年と月を取得
+      const year = new Date(date).getFullYear();
+      const month = String(new Date(date).getMonth() + 1).padStart(2, "0");
+
+      return {
+        date,
+        yearMonth: `${year}-${month}`,
+        workTime,
+        startDatetime: startAt,
+        endDatetime: endAt,
+      };
+    }
+  );
+
+  const dateValues = convertedShifts.map((value) => {
+    return {
+      parent: { database_id: process.env.REACT_APP_NOTION_DB_ENDPOINT_ID },
+      properties: {
+        名前: {
+          title: [
+            {
+              text: { content: homeWorkerName },
+            },
+          ],
+        },
+        勤務開始時間: {
+          date: { start: value.startDatetime },
+        },
+        勤務終了時間: {
+          date: { start: value.endDatetime },
+        },
+        勤務時間: {
+          number: value.workTime,
+        },
+        勤務月: {
+          rich_text: [
+            {
+              type: "text",
+              text: {
+                content: value.yearMonth,
+              },
+            },
+          ],
+        },
+      },
+    };
+  });
+
+  try {
+    const NOTION_API_URL = "https://api.notion.com/v1/pages";
+    await Promise.all(
+      dateValues.map(async (dateValue) => {
+        await axios.post(NOTION_API_URL, dateValue, {
+          headers: {
+            Authorization: `Bearer ${process.env.REACT_APP_NOTION_SHIFT_DB_API_TOKEN}`,
+            "Content-Type": "application/json",
+            "Notion-Version": "2022-06-28", // Notion APIバージョン
+          },
+        });
+      })
+    );
+  } catch (e) {
+    console.error(e);
+  }
+
+  res.json({ message: "Success" });
+});
+
 // ローカル環境で動作確認したい場合はコメントアウトを外すとサーバーが立ち上がる
 // const PORT = process.env.PORT || 3005;
 // app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
