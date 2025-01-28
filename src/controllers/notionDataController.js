7f297b86822b7da74d778dfc1fc1242b651dbb9d
mura-work <murata.naoya.work@gmail.com>
2025-01-29 02:23:46 +0900

    在宅ワーカー登録処理の実装

--- src/controllers/notionDataController.js
+++ src/controllers/notionDataController.js
@@ -193,117 +193,140 @@ exports.getHomeWorkers = async (req, res) => {
 
 // 在宅ワーカーをDBに登録する
 exports.postRegisterNewHomeWorker = async (req, res) => {
-  const newHomeWorkerValue = req.body.newHomeWorkerValue;
+  try {
+    // 在宅ワーカーの人数を取得
+    const response = await axios.post(
+      `https://api.notion.com/v1/databases/${process.env.REACT_APP_NOTION_HOME_WORKER_MANAGEMENT_DB_ENDPOINT_ID}/query`,
+      {},
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.REACT_APP_NOTION_API_TOKEN}`,
+          "Notion-Version": "2022-06-28",
+          "Content-Type": "application/json",
+        },
+      }
+    );
 
-  const requestParams = {
-    parent: {
-      database_id:
-        process.env.REACT_APP_NOTION_HOME_WORKER_MANAGEMENT_DB_ENDPOINT_ID,
-    },
-    properties: {
-      名前: {
-        rich_text: [
-          {
-            type: "text",
-            text: { content: newHomeWorkerValue.name },
-          },
-        ],
+    const homeWorkerCount = response.data.results.length;
+
+    const newHomeWorkerValue = req.body.newHomeWorkerValue;
+
+    const requestParams = {
+      parent: {
+        database_id:
+          process.env.REACT_APP_NOTION_HOME_WORKER_MANAGEMENT_DB_ENDPOINT_ID,
       },
-      "名前（カナ）": {
-        rich_text: [
-          {
-            type: "text",
-            text: { content: newHomeWorkerValue.nameKana },
+      properties: {
+        メンバーID: {
+          title: [
+            {
+              text: { content: `ZW-${homeWorkerCount + 1}` },
+            },
+          ],
+        },
+        名前: {
+          rich_text: [
+            {
+              type: "text",
+              text: { content: newHomeWorkerValue.name },
+            },
+          ],
+        },
+        "名前（カナ）": {
+          rich_text: [
+            {
+              type: "text",
+              text: { content: newHomeWorkerValue.nameKana },
+            },
+          ],
+        },
+        個人メールアドレス: {
+          rich_text: [
+            {
+              type: "text",
+              text: { content: newHomeWorkerValue.personalAddress },
+            },
+          ],
+        },
+        個人電話番号: {
+          rich_text: [
+            {
+              type: "text",
+              text: { content: newHomeWorkerValue.phoneNumber },
+            },
+          ],
+        },
+        振込先銀行名: {
+          rich_text: [
+            {
+              type: "text",
+              text: { content: newHomeWorkerValue.bankName },
+            },
+          ],
+        },
+        振込先支店名: {
+          rich_text: [
+            {
+              type: "text",
+              text: { content: newHomeWorkerValue.branchName },
+            },
+          ],
+        },
+        振込先口座種別: {
+          select: {
+            name: newHomeWorkerValue.accountType,
           },
-        ],
-      },
-      個人メールアドレス: {
-        rich_text: [
-          {
-            type: "text",
-            text: { content: newHomeWorkerValue.personalAddress },
-          },
-        ],
-      },
-      個人電話番号: {
-        rich_text: [
-          {
-            type: "text",
-            text: { content: newHomeWorkerValue.phoneNumber },
-          },
-        ],
-      },
-      振込先銀行名: {
-        rich_text: [
-          {
-            type: "text",
-            text: { content: newHomeWorkerValue.bankName },
-          },
-        ],
-      },
-      振込先支店名: {
-        rich_text: [
-          {
-            type: "text",
-            text: { content: newHomeWorkerValue.branchName },
-          },
-        ],
-      },
-      振込先口座種別: {
-        select: {
-          name: newHomeWorkerValue.accountType,
+        },
+        口座番号: {
+          rich_text: [
+            {
+              type: "text",
+              text: { content: newHomeWorkerValue.accountNumber },
+            },
+          ],
+        },
+        口座名義人: {
+          rich_text: [
+            {
+              type: "text",
+              text: { content: newHomeWorkerValue.accountHolder },
+            },
+          ],
+        },
+        生年月日: {
+          rich_text: [
+            {
+              type: "text",
+              text: { content: newHomeWorkerValue.birthday },
+            },
+          ],
+        },
+        住民票所在地: {
+          rich_text: [
+            {
+              type: "text",
+              text: { content: newHomeWorkerValue.address },
+            },
+          ],
+        },
+        住まい住所: {
+          rich_text: [
+            {
+              type: "text",
+              text: { content: newHomeWorkerValue.home },
+            },
+          ],
+        },
+        最短勤務可能日: {
+          date: { start: newHomeWorkerValue.earliestStartDate },
+        },
+        "稼働想定時間（月）": {
+          number: newHomeWorkerValue.estimatedWorkingHoursPerMonth,
         },
       },
-      口座番号: {
-        rich_text: [
-          {
-            type: "text",
-            text: { content: newHomeWorkerValue.accountNumber },
-          },
-        ],
-      },
-      口座名義人: {
-        rich_text: [
-          {
-            type: "text",
-            text: { content: newHomeWorkerValue.accountHolder },
-          },
-        ],
-      },
-      生年月日: {
-        rich_text: [
-          {
-            type: "text",
-            text: { content: newHomeWorkerValue.birthday },
-          },
-        ],
-      },
-      住民票所在地: {
-        rich_text: [
-          {
-            type: "text",
-            text: { content: newHomeWorkerValue.address },
-          },
-        ],
-      },
-      住まい住所: {
-        rich_text: [
-          {
-            type: "text",
-            text: { content: newHomeWorkerValue.home },
-          },
-        ],
-      },
-      最短勤務可能日: {
-        date: { start: newHomeWorkerValue.earliestStartDate },
-      },
-      "稼働想定時間（月）": {
-        number: newHomeWorkerValue.estimatedWorkingHoursPerMonth,
-      },
-    },
-  };
+    };
 
-  try {
+    // 在宅ワーカー管理DBに登録
     await axios.post(process.env.REACT_APP_NOTION_API_URL, requestParams, {
       headers: {
         Authorization: `Bearer ${process.env.REACT_APP_NOTION_API_TOKEN}`,
@@ -311,6 +334,47 @@ exports.postRegisterNewHomeWorker = async (req, res) => {
         "Notion-Version": "2022-06-28", // Notion APIバージョン
       },
     });
+
+    // メンバー用DBに登録
+    const memberRequestParams = {
+      parent: {
+        database_id:
+          process.env.REACT_APP_NOTION_HOME_WORK_MEMBER_DB_ENDPOINT_ID,
+      },
+      properties: {
+        メンバーID: {
+          title: [
+            {
+              text: { content: `ZW-${homeWorkerCount + 1}` },
+            },
+          ],
+        },
+        名前: {
+          rich_text: [
+            {
+              type: "text",
+              text: { content: newHomeWorkerValue.name },
+            },
+          ],
+        },
+        入社日: {
+          date: { start: newHomeWorkerValue.earliestStartDate },
+        },
+      },
+    };
+
+    await axios.post(
+      process.env.REACT_APP_NOTION_API_URL,
+      memberRequestParams,
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.REACT_APP_NOTION_API_TOKEN}`,
+          "Content-Type": "application/json",
+          "Notion-Version": "2022-06-28", // Notion APIバージョン
+        },
+      }
+    );
+
     res.json({ message: "Success" });
   } catch (e) {
     console.error(e);
