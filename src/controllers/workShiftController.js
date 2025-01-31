b2f3fde421fcf6b567223eaef01aa3597da2e58b
mura-work <murata.naoya.work@gmail.com>
2025-01-31 20:14:34 +0900

    シフト管理フォームの修正

---
+++ src/controllers/workShiftController.js
@@ -0,0 +1,90 @@
+const axios = require("axios");
+
+// シフト管理DBに保存する
+exports.postShiftManagement = async (req, res) => {
+  const shiftRequestValues = req.body.shiftRequestValues;
+  const homeWorkerName = req.body.homeWorkerName;
+  const convertedShifts = shiftRequestValues.map(
+    ({ date, startTime, endTime }) => {
+      // "2025-01-01 09:00" などに結合して返す
+      const startAt = `${date} ${startTime}`;
+      const endAt = `${date} ${endTime}`;
+
+      // 差分をミリ秒で計算
+      const diffMilliseconds = Math.abs(
+        new Date(`${date} ${endTime}`).getTime() -
+          new Date(`${date} ${startTime}`).getTime()
+      );
+
+      // ミリ秒を時間に変換
+      const workTime = diffMilliseconds / (1000 * 60 * 60);
+
+      // Dateオブジェクトを受け取り、年と月を取得
+      const year = new Date(date).getFullYear();
+      const month = String(new Date(date).getMonth() + 1).padStart(2, "0");
+
+      return {
+        date,
+        yearMonth: `${year}-${month}`,
+        workTime,
+        startDatetime: startAt,
+        endDatetime: endAt,
+      };
+    }
+  );
+  console.log(req.body);
+  const dateValues = convertedShifts.map((value) => {
+    return {
+      parent: {
+        database_id:
+          process.env.REACT_APP_NOTION_HOME_WORKER_SHIFT_DB_ENDPOINT_ID,
+      },
+      properties: {
+        名前: {
+          title: [
+            {
+              text: { content: homeWorkerName },
+            },
+          ],
+        },
+        勤務開始時間: {
+          date: { start: value.startDatetime },
+        },
+        勤務終了時間: {
+          date: { start: value.endDatetime },
+        },
+        勤務時間: {
+          number: value.workTime,
+        },
+        勤務月: {
+          rich_text: [
+            {
+              type: "text",
+              text: {
+                content: value.yearMonth,
+              },
+            },
+          ],
+        },
+      },
+    };
+  });
+
+  try {
+    await Promise.all(
+      dateValues.map(async (dateValue) => {
+        await axios.post(process.env.REACT_APP_NOTION_API_URL, dateValue, {
+          headers: {
+            Authorization: `Bearer ${process.env.REACT_APP_NOTION_API_TOKEN}`,
+            "Content-Type": "application/json",
+            "Notion-Version": "2022-06-28", // Notion APIバージョン
+          },
+        });
+      })
+    );
+  } catch (e) {
+    console.error(e);
+  }
+
+  res.json({ message: "Success" });
+};
--- src/routers/notionData.js
+++ src/routers/notionData.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const notionDataController = require("../controllers/notionDataController");
 const workAttendanceController = require("../controllers/workAttendanceController");
+const workShiftController = require("../controllers/workShiftController");
 
 // 法人名（法人番号）の取得
 router.get("client-companies", notionDataController.getClientCompanies);
@@ -13,7 +14,7 @@ router.get("/sales-employees", notionDataController.getSalesEmployees);
 router.get("/products", notionDataController.getProducts);
 
 // シフト管理DBに保存する
-router.post("/shift-management", notionDataController.postShiftManagement);
+router.post("/shift-management", workShiftController.postShiftManagement);
 
 // 在宅ワーカーの取得
 router.get("/home-workers", notionDataController.getHomeWorkers);
