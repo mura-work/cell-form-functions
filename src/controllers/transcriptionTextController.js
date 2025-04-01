a6865f4c64173d1b40f3ab5550da43c84c515db9
mura-work <murata.naoya.work@gmail.com>
2025-04-02 08:53:09 +0900

    一旦コミット

--- src/controllers/transcriptionTextController.js
+++ src/controllers/transcriptionTextController.js
@@ -6,11 +6,12 @@ exports.summarizeTranscriptionText = async (req, res) => {
     const response = await axios.post(
       "https://api.openai.com/v1/chat/completions",
       {
-        model: "gpt-4o",
+        model: "o1-preview",
         messages: [
           {
-            role: "system",
-            content: `下記のテキストは議事録の文字起こしです。以下の指示に従い、できるだけ原文の意味を変えずに自然で読みやすい日本語の文章体に変換してください。
+            role: "user",
+            content: `
+						下記のテキストは議事録の文字起こしです。以下の指示に従い、できるだけ原文の意味を変えずに自然で読みやすい日本語の文章体に変換してください。
 
 							1. 箇条書きや箇条書き風の表現は避け、段落を使って文章を構成してください。
 							2. 話し言葉の特徴（例：「ええと」「あの」「えー」など）は適宜修正し、よりフォーマルな表現に置き換えてください。ただし、話し言葉による強調のニュアンスはできる限り保持してください。
@@ -18,18 +19,17 @@ exports.summarizeTranscriptionText = async (req, res) => {
 							4. 文脈が明確になるように、必要に応じて接続詞や指示語を追加してください。
 							5. 変換後のテキストは、元のトランスクリプトの内容を一切要約せず、すべて網羅してください。
 							6. 明らかに話題が変わったタイミングがあれば、【〇〇】のようにラベルを付けて段落を区切り、セグメントごとに整理してください。
-						`,
-          },
-          {
-            role: "user",
-            content: `次の文章は音声ファイルの文字起こしテキストです。上記のルールを元に、文字起こしテキストを要約してください。
-						出力形式を必ず守ってください。
-						【文字起こしテキスト】
-						${transcriptionText}`,
+
+							上記のルールを元に、文字起こしテキストを要約してください。
+							出力形式を必ず守ってください。
+
+							【文字起こしテキスト】
+							${transcriptionText}
+					`,
           },
         ],
-        temperature: 0,
-        max_tokens: 8192,
+        // temperature: 0,
+        max_tokens: 16384,
       },
       {
         headers: {
