9cb6fa0e9549a546ed39a19902d4cbf182d4e7da
mura-work <murata.naoya.work@gmail.com>
2025-05-04 11:55:31 +0900

    稼働開始フォームから保存するメソッドの作成

--- src/controllers/lark/homeWorkerController.js
+++ src/controllers/lark/homeWorkerController.js
@@ -1,6 +1,6 @@
 const axios = require("axios");
 
-// 1. app_access_tokenを取得
+// app_access_tokenを取得
 const getAppAccessToken = async () => {
   const url =
     "https://open.larksuite.com/open-apis/auth/v3/app_access_token/internal";
@@ -17,11 +17,16 @@ const getAppAccessToken = async () => {
   return res.data.app_access_token;
 };
 
-// 2. テーブルレコード一覧を取得
-const getTableRecords = async (appAccessToken) => {
+// テーブルレコード一覧を取得
+const getTableRecords = async (appAccessToken, includeRetired = false) => {
   const appToken = "EyI0b8tEea9PxQsFhiyjdoLbpV3";
   const tableId = "tbl8Zef1I5HocKbP";
-  const url = `https://open.larksuite.com/open-apis/bitable/v1/apps/${appToken}/tables/${tableId}/records?filter=CurrentValue.[退職フラグ]="在職中"&page_size=20`;
+
+  const endpoint = `https://open.larksuite.com/open-apis/bitable/v1/apps/${appToken}/tables/${tableId}`;
+  const filter = includeRetired
+    ? `sort=["メンバーID ASC"]&page_size=20`
+    : `filter=CurrentValue.[退職フラグ]="在職中"&sort=["メンバーID ASC"]&page_size=20`;
+  const url = `${endpoint}/records?${filter}`;
   const res = await axios.get(url, {
     headers: {
       Authorization: `Bearer ${appAccessToken}`,
@@ -30,6 +35,7 @@ const getTableRecords = async (appAccessToken) => {
   return res.data;
 };
 
+// 在職中のメンバー一覧を取得
 exports.getHomeWorkers = async (_, res) => {
   const appAccessToken = await getAppAccessToken();
   const tableRecords = await getTableRecords(appAccessToken);
@@ -42,4 +48,70 @@ exports.getHomeWorkers = async (_, res) => {
   }));
 
   res.json(records);
+};
+
+// RemoteSalesのデータを新規作成
+exports.createRemoteSalesData = async (req, res) => {
+  const appAccessToken = await getAppAccessToken();
+
+  // メンバー一覧を取得
+  const homeWorkers = await getTableRecords(appAccessToken, true);
+
+  // 最後に登録したメンバーのメンバーIDを取得
+  const lastHomeWorkerId = homeWorkers.data.items.at(-1).fields["メンバーID"];
+
+  // そのIDの数字の部分を+1する
+  const newHomeWorkerId = lastHomeWorkerId.replace(/RS-/, "");
+  const newHomeWorkerIdNumber = parseInt(newHomeWorkerId) + 1;
+  const newHomeWorkerIdWithPrefix = `RS-${newHomeWorkerIdNumber}`;
+
+  // そのID+リクエストパラメータを使用してレコードを作成
+  const params = req.body.newHomeWorkerValue;
+  const fields = {
+    メンバーID: newHomeWorkerIdWithPrefix,
+    名前: params.name,
+    "名前（カナ）": params.nameKana,
+    個人メールアドレス: params.personalAddress,
+    個人電話番号: params.phoneNumber,
+    振込先銀行名: params.bankName,
+    振込先支店名: params.branchName,
+    振込先口座種別: params.accountType,
+    口座番号: params.accountNumber,
+    口座名義人: params.accountHolder,
+    生年月日: params.birthday,
+    住民票所在地: params.address,
+    住まい住所: params.home,
+    最短勤務可能日: params.earliestStartDate,
+    "稼働想定時間（月）": params.estimatedWorkingHoursPerMonth,
+    退職フラグ: "在職中",
+  };
+
+  // 時給DBにレコードを追加する
+  const appToken = "EyI0b8tEea9PxQsFhiyjdoLbpV3";
+  const tableId = "tbl8Zef1I5HocKbP";
+
+  const endpoint = `https://open.larksuite.com/open-apis/bitable/v1/apps/${appToken}/tables/${tableId}/records`;
+  const response = await axios.post(
+    endpoint,
+    {
+      fields,
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${appAccessToken}`,
+        "Content-Type": "application/json",
+      },
+    }
+  );
+
+  if (response.data.error) {
+    res.status(400).json({
+      message: "エラーが発生しました。再度登録してください。",
+      ...response.data,
+    });
+  } else {
+    res.status(200).json({
+      message: "登録が完了しました。",
+    });
+  }
 };
--- src/routers/larkData.js
+++ src/routers/larkData.js
@@ -4,5 +4,9 @@ const larkDataController = require("../controllers/lark/homeWorkerController");
 
 // Larkから従業員データを取得
 router.get("/home-workers", larkDataController.getHomeWorkers);
+router.post(
+  "/create-remote-sales-data",
+  larkDataController.createRemoteSalesData
+);
 
 module.exports = router;
