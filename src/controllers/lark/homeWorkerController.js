ffd7cd7c203d8211107864d17858ac3f081cf9fb
mura-work <murata.naoya.work@gmail.com>
2025-05-04 10:40:32 +0900

    対象のテーブルを修正

--- src/controllers/lark/homeWorkerController.js
+++ src/controllers/lark/homeWorkerController.js
@@ -19,8 +19,8 @@ const getAppAccessToken = async () => {
 
 // 2. テーブルレコード一覧を取得
 const getTableRecords = async (appAccessToken) => {
-  const appToken = "TsdJbCkPIa2N3osmoCVj2aYypDg";
-  const tableId = "tblGBaTJASX5soWr";
+  const appToken = "EyI0b8tEea9PxQsFhiyjdoLbpV3";
+  const tableId = "tbl8Zef1I5HocKbP";
   const url = `https://open.larksuite.com/open-apis/bitable/v1/apps/${appToken}/tables/${tableId}/records?page_size=20`;
   const res = await axios.get(url, {
     headers: {
