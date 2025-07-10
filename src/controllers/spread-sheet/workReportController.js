bc3ff0b97068e1cbbeab7089cc2493909d88a75a
mura-work <murata.naoya.work@gmail.com>
2025-07-10 13:20:26 +0900

    稼働報告のbackend実装

--- src/controllers/spread-sheet/workReportController.js
+++ src/controllers/spread-sheet/workReportController.js
@@ -1,11 +1,11 @@
 const axios = require("axios");
 
-exports.createShift = async (req, res) => {
+exports.createWorkReport = async (req, res) => {
   try {
     const params = req.body;
 
     const fieldMapping = {
-      type: "shift", // typeでメソッドを分岐させる
+      type: "workReport", // typeでメソッドを分岐させる
       ...params,
     };
     console.log({ fieldMapping });
@@ -26,10 +26,10 @@ exports.createShift = async (req, res) => {
     }
 
     res.json({
-      message: "シフト登録が完了しました。",
+      message: "稼働報告登録が完了しました。",
     });
   } catch (error) {
-    console.error("シフト登録処理エラー:", error);
+    console.error("稼働報告登録処理エラー:", error);
     res.status(500).json({
       message: "システムエラーが発生しました。",
       error: error.message,
@@ -37,14 +37,14 @@ exports.createShift = async (req, res) => {
   }
 };
 
-exports.getShiftByEmail = async (req, res) => {
+exports.getWorkReportByEmail = async (req, res) => {
   try {
     const email = req.query.email;
-    const url = `${process.env.REACT_APP_REMOTE_SALES_MANAGEMENT_ENDPOINT}?type=shift&email=${email}`;
-    const shifts = await axios.get(url).then((r) => r.data);
-    res.json(shifts);
+    const url = `${process.env.REACT_APP_REMOTE_SALES_MANAGEMENT_ENDPOINT}?type=workReport&email=${email}`;
+    const workReports = await axios.get(url).then((r) => r.data);
+    res.json(workReports);
   } catch (error) {
-    console.error("シフト詳細取得処理エラー:", error);
+    console.error("稼働報告詳細取得処理エラー:", error);
     res.status(500).json({
       message: "システムエラーが発生しました。",
       error: error.message,
@@ -52,13 +52,13 @@ exports.getShiftByEmail = async (req, res) => {
   }
 };
 
-exports.getShifts = async (_, res) => {
+exports.getWorkReports = async (_, res) => {
   try {
-    const url = `${process.env.REACT_APP_REMOTE_SALES_MANAGEMENT_ENDPOINT}?type=shift`;
-    const shifts = await axios.get(url).then((r) => r.data);
-    res.json(shifts);
+    const url = `${process.env.REACT_APP_REMOTE_SALES_MANAGEMENT_ENDPOINT}?type=workReport`;
+    const workReports = await axios.get(url).then((r) => r.data);
+    res.json(workReports);
   } catch (error) {
-    console.error("シフト一覧取得処理エラー:", error);
+    console.error("稼働報告一覧取得処理エラー:", error);
     res.status(500).json({
       message: "システムエラーが発生しました。",
       error: error.message,
--- src/routers/spreadsheetRouter.js
+++ src/routers/spreadsheetRouter.js
@@ -3,14 +3,23 @@ const router = express.Router();
 const joiningCompanyController = require("../controllers/spread-sheet/joiningCompanyController");
 const newContractEmployeeController = require("../controllers/spread-sheet/newContractEmployeeController");
 const shiftController = require("../controllers/spread-sheet/shiftController");
+const workReportController = require("../controllers/spread-sheet/workReportController");
 
+// 契約開始時回答フォーム
 router.post("/joining-company", joiningCompanyController.createJoiningCompany);
 router.post(
   "/new-contract-employee",
   newContractEmployeeController.createNewContractEmployee
 );
+
+// シフト
 router.post("/shifts", shiftController.createShift);
 router.get("/shifts", shiftController.getShifts);
 router.get("/shift", shiftController.getShiftByEmail);
+
+// 稼働報告
+router.post("/work-reports", workReportController.createWorkReport);
+router.get("/work-reports", workReportController.getWorkReports);
+router.get("/work-report", workReportController.getWorkReportByEmail);
 
 module.exports = router;
